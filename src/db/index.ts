6cae95d34791584480cec84a89e65bdb8f9a52b8
Nimeesh <bagwe.nimeesh4@gmail.com>
2025-06-27 16:47:21 -0400

    02-configured Drizzle ORM with Neon and setup CLI support

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) 
+    {
+  throw new Error("DATABASE_URL is not set in the environment variables.");
+}
+
+export const db = drizzle(connectionString);
+
